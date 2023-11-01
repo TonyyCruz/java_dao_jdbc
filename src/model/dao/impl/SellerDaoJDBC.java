55f4b54b79b9d38e0779211c097a429b1527a1d4
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-11-01 14:48:29 -0300

    feat: implementa metodos auxiliares para instanciar seller e department

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 import db.DB;
 import db.DBException;
@@ -48,17 +49,8 @@ public class SellerDaoJDBC implements SellerDao {
       resultSet = prepStatement.executeQuery();
       if (!resultSet.next())
         return null;
-      Department department = new Department();
-      department.setId(resultSet.getInt("DepartmentId"));
-      department.setName(resultSet.getString("DepName"));
-      Seller seller = new Seller();
-      seller.setId(resultSet.getInt("Id"));
-      seller.setName(resultSet.getString("Name"));
-      seller.setEmail(resultSet.getString("Email"));
-      seller.setBirthDate(resultSet.getDate("BirthDate"));
-      seller.setBaseSalary(resultSet.getDouble("BaseSalary"));
-      seller.setDepartment(department);
-      return seller;
+      Department department = departmentInstantiate(resultSet);
+      return sellerInstantiate(resultSet, department);
     } catch (SQLException e) {
       throw new DBException(e.getMessage());
     } finally {
@@ -73,4 +65,18 @@ public class SellerDaoJDBC implements SellerDao {
     return null;
   }
 
+  private Department departmentInstantiate(ResultSet resultSet) throws SQLException {
+    int id = resultSet.getInt("DepartmentId");
+    String name = resultSet.getString("DepName");
+    return new Department(id, name);
+  }
+
+  private Seller sellerInstantiate(ResultSet resultSet, Department department) throws SQLException {
+    int id = resultSet.getInt("Id");
+    String name = resultSet.getString("Name");
+    String email = resultSet.getString("Email");
+    Date birthDate = resultSet.getDate("BirthDate");
+    double salary = resultSet.getDouble("BaseSalary");
+    return new Seller(id, name, email, birthDate, salary, department);
+  }
 }
